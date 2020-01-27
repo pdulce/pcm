409cb39b2c95f29a012048325df5e0481adb9137
pdulce <59163688+pdulce@users.noreply.github.com>
2020-01-27 13:04:07 +0100

    Complete - task : Eliminar tildes y enyes de los fuentes

---
+++ contextPropModel/src/pcm/common/stats/regression/VariableDecisoraContinua.java
@@ -0,0 +1,12 @@
+package pcm.common.stats.regression;
+
+import java.util.HashMap;
+
+public class VariableDecisoraContinua extends VariableDecisora{
+	public VariableDecisoraContinua(final String name_, final Range range_){
+		setName(name_);
+		setRange(range_);
+		setLiteralMappingTo(new HashMap<String, Integer>());
+		setCategories(0);
+	}
+}
