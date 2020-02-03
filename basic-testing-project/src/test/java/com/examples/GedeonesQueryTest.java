f5cb3bec2e18b64cc566e06fedb74a15091791f4
pedro <pedro@pedro-PORTEGE-R700>
2020-02-03 20:27:03 +0100

    # WARNING: head commit changed in the meantime
    : Lanzar tests con Selenium, Canoo WebTests u otras
    
    Task-Url:

--- basic-testing-project/src/test/java/com/examples/GedeonesQueryTest.java
+++ basic-testing-project/src/test/java/com/examples/GedeonesQueryTest.java
@@ -2,14 +2,14 @@ package com.examples;
 
 import static org.openqa.selenium.support.ui.ExpectedConditions.presenceOfElementLocated;
 
+import java.util.List;
+
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 
 import org.openqa.selenium.By;
-//import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -18,7 +18,7 @@ import junit.framework.TestCase;
 /***
  * Tests login feature for SeleniumHQ WebDriver
  */
-public class LoginTest extends TestCase{
+public class GedeonesQueryTest extends TestCase{
 	
 	static {
 		System.setProperty("webdriver.gecko.driver", "/home/pedro/gecko/geckodriver");
@@ -29,47 +29,56 @@ public class LoginTest extends TestCase{
      *
      * @param testName name of the test case
      */
-    public LoginTest( )
+    public GedeonesQueryTest( )
     {
         super( "loginTest" );
 	
     }
 
-	
 	@Test
-	public void testLoginExists() {
-		
+	public void queryTest() {
 		WebDriver driver = WebdriverObject.getWebDriverInstance();
         try {
-            
+        	
             WebElement entryUserForm = driver.findElement(By.name("entryForm.user"));
             WebElement entryPaswdForm = driver.findElement(By.name("entryForm.password"));
                         
-            Assert.assertTrue(entryUserForm.isDisplayed());
-            Assert.assertTrue(entryPaswdForm.isDisplayed());
-            
             entryUserForm.sendKeys("admin");
             entryPaswdForm.sendKeys("admin");
             
             WebElement submitFormElement = driver.findElement(By.id("submitForm"));
             submitFormElement.click();
             
+            //"dhtmlgoodies_treeNode20": id del nodo Gedeones cuando entramos como Administrador
+            WebDriverWait waitForTree = new WebDriverWait(driver, Long.valueOf(5));
+            waitForTree.until(ExpectedConditions.visibilityOfElementLocated(By.id("dhtmlgoodies_tree")));//o la Xpath "//UL[@id="dhtmlgoodies_tree"]
+            WebElement arbolNavegacion = waitForTree.until(presenceOfElementLocated(By.id("dhtmlgoodies_tree")));
+            Assert.assertTrue(arbolNavegacion.isDisplayed());
+           
+            WebElement seguimientoFolder = arbolNavegacion.findElement(By.xpath("//A[@id='dhtmlgoodies_treeNode20']"));
+            Assert.assertTrue(seguimientoFolder.getText().contains("Seguimiento"));
+           
+            seguimientoFolder.click();
+            
+            WebElement hrefGEDEONES = seguimientoFolder.findElement(By.xpath("//A[@id='dhtmlgoodies_treeNode21']"));
+            Assert.assertTrue(hrefGEDEONES.getText().contains("GEDEON"));
+            hrefGEDEONES.click();
+            
             WebDriverWait wait = new WebDriverWait(driver, Long.valueOf(20));
             wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("principal")));
             WebElement divPral = wait.until(presenceOfElementLocated(By.xpath("//div[@id='principal']")));
-            //WebElement divPral =wait.until(presenceOfElementLocated(By.id("principal")));
-            Assert.assertTrue(divPral.isDisplayed());
+            List<WebElement> countResults = divPral.findElements(By.tagName("LEGEND"));
             
-            //System.out.println(divPral.getText());
-         
-            Assert.assertTrue(divPral.getText().contains("Bienvenid@ Administrador, comience a navegar por el árbol lateral de servicios"));
+            Assert.assertTrue(countResults.size() > 0);
+            
+            Assert.assertTrue(divPral.getText().contains("Resultados del  1 al"));
                         
         }catch (Exception exc) {
         	System.out.println("Error " + exc.getMessage());
         	exc.printStackTrace();
-        } //finally {
-        //  driver.quit();
-        //}
+        } finally {
+            driver.quit();
+        }
 		
 	}
 
---
+++ basic-testing-project/src/test/java/com/examples/WebdriverObject.java
@@ -0,0 +1,26 @@
+package com.examples;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class WebdriverObject {
+	static {
+		System.setProperty("webdriver.gecko.driver", "/home/pedro/gecko/geckodriver");
+	}
+	private static WebDriver driver;
+	
+	public static WebDriver getWebDriverInstance() {
+
+        try {
+        	if (driver == null) {
+        		driver=new FirefoxDriver();
+        	}
+            driver.get("http://localhost:8081/sg/prjManager");
+            return driver;
+        }catch (Throwable exc) {
+        	return null;
+        }
+        
+	}
+	
+}
