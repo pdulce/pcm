b3c9488c5cbd567f81cff3ec9deb0ed60e98fcc7
pdulce <59163688+pdulce@users.noreply.github.com>
2020-02-06 17:11:44 +0100

    Complete - task : Eliminar tildes y enyes de los fuentes

---
+++ basic-testing-project/src/test/java/com/examples/PruebaSelenium.java
@@ -0,0 +1,27 @@
+package test.java.com.examples;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class PruebaSelenium {
+
+	
+	public static void main(String[] args){
+				
+    	WebDriver driver = WebdriverObject.getWebDriverInstance();
+		try {
+			
+			WebElement ele2Rearch =  driver.findElement(By.name("entryForm.user"));
+			
+			System.out.println("Texto: " + ele2Rearch.getText());
+			
+		} catch (Exception exc) {
+			System.out.println("Error in testLoginErrUser: " + exc.getMessage());
+			exc.printStackTrace();
+		}finally {
+			WebdriverObject.reinitializeDriver();
+		}
+	}
+	
+}
