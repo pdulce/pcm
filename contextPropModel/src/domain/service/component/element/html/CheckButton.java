df78da9017d8d747366fb1297a1fad154ca20bf2
esdu-er <esdu-er@SAG-J5688H2.eur.ad.sag>
2020-04-24 12:51:52 +0200

    .

--- contextPropModel/src/domain/service/component/element/html/CheckButton.java
+++ contextPropModel/src/domain/service/component/element/html/CheckButton.java
@@ -31,7 +31,7 @@ public class CheckButton extends GenericInput {
 
 	public CheckButton() {
 		this.setType(ICtrl.CHECK_TYPE);
-		this.setClassId("optionStyle");
+		this.setClassId("checkmarkbox");
 	}
 
 	public String getInternalValue() {
@@ -59,16 +59,33 @@ public class CheckButton extends GenericInput {
 	}
 
 	@Override
+	/** Genera:
+	 * <label class="checkboxcontainer">One
+  			<input type="checkbox" checked="checked">
+  			<span class="checkmark"></span>
+	   </label>
+	 * 
+	 */
 	public String toHTML(final String descrTraduced_, final Collection<String> values_) {
+		
+		
+		final StringBuilder cad = new StringBuilder("<label class=\"checkboxcontainer\">");
+		cad.append(descrTraduced_).append("&nbsp;&nbsp;");
+		
+		final StringBuilder input = new StringBuilder();
+		input.append(GenericInput.BEGIN_INPUT);
+		input.append(this.getType()).append(GenericInput.ATTR_VAL).append(this.internalValue);
+		input.append(PCMConstants.END_COMILLAS).append(this.getId()).append(this.getName());
 		String valueOfOption = this.internalValue.indexOf(PCMConstants.EQUALS) == -1 ? this.internalValue : this.internalValue.split(PCMConstants.EQUALS)[1];
 		boolean isChecked = values_.isEmpty() ? this.isCheckedByDefault(): values_.contains(valueOfOption);
-		final StringBuilder cad = new StringBuilder(GenericInput.BEGIN_INPUT);
-		cad.append(this.getType()).append(GenericInput.ATTR_VAL).append(this.internalValue).
-		append(PCMConstants.END_COMILLAS).append(this.getId()).append(this.getName()).append(this.getDisabled()).
-		append(isChecked ? CheckButton.CHECKED_ATTR_VALUE : PCMConstants.EMPTY_).
-		append(this.getOnClick()).append(this.getOnMouseOut()).append(this.getOnDbClick()).append(this.getOnMouseOver()).
-		append("/>").append("<label").append(this.getClassId()).append(" for=" + this.getId().replaceFirst(" id=", "") + "><span>").
-		append("&nbsp;").append(descrTraduced_).append("&nbsp;").append("</span>").append("</label>");
+		input.append(this.getDisabled()).append(isChecked ? CheckButton.CHECKED_ATTR_VALUE : PCMConstants.EMPTY_);
+		input.append(this.getOnClick()).append(this.getOnMouseOut()).append(this.getOnDbClick()).append(this.getOnMouseOver()).append(">");
+		
+		cad.append(input);
+		
+		cad.append("<span ").append(this.getClassId()).append("></span>").
+		
+		append("</label>");
 		return cad.toString();
 	}
 
--- contextPropModel/src/domain/service/component/element/html/RadioButton.java
+++ contextPropModel/src/domain/service/component/element/html/RadioButton.java
@@ -24,10 +24,11 @@ public class RadioButton extends GenericInput {
 	private String internalValue = PCMConstants.EMPTY_, description = null;
 	private boolean checkedByDefault = false;
 	
-	public RadioButton(){
-		this.setClassId("optionStyle");
-	}
+	public RadioButton() {
+		this.setType(ICtrl.RADIO_TYPE);
+		this.setClassId("checkmarkradio");
 	
+	}
 	public String getDescription() {
 		return this.description;
 	}
@@ -50,12 +51,25 @@ public class RadioButton extends GenericInput {
 	}
 
 	@Override
-	public String toHTML(final String descrTraduced, final Collection<String> values_) {
-		final StringBuilder cad = new StringBuilder(IHtmlElement.BLANCO);
-		cad.append(GenericInput.BEGIN_INPUT).append(this.getType()).append(this.getClassId()).append(GenericInput.ATTR_VAL);
-		cad.append(this.internalValue).append(PCMConstants.END_COMILLAS).append(this.getId()).append(this.getName());
+	/***
+	 * generates:
+	 * <label class="radiogroupcontainer">One
+  			<input type="radio" checked="checked" name="radio">
+  			<span class="checkmark"></span>
+	   </label>
+	 * 
+	 */
+	public String toHTML(final String descrTraduced_, final Collection<String> values_) {
+		
+		final StringBuilder cad = new StringBuilder("<label class=\"radiogroupcontainer\">");
+		cad.append(descrTraduced_).append("&nbsp;&nbsp;");
+		
+		//begins input
+		
+		final StringBuilder input = new StringBuilder();
+		input.append(GenericInput.BEGIN_INPUT).append(this.getType()).append(this.getClassId()).append(GenericInput.ATTR_VAL);
+		input.append(this.internalValue).append(PCMConstants.END_COMILLAS).append(this.getId()).append(this.getName());
 		// hacemos la conversion de boolean a int o viceversa:
-
 		List<String> newvalues = new ArrayList<String>();
 		if (!values_.isEmpty()) {
 			this.internalValue = this.internalValue.replaceFirst("false", "0");
@@ -70,13 +84,20 @@ public class RadioButton extends GenericInput {
 		}
 		String valueOfOption = this.internalValue.indexOf(PCMConstants.EQUALS) == -1 ? this.internalValue : this.internalValue.split(PCMConstants.EQUALS)[1];
 		boolean isChecked= newvalues.isEmpty() ? this.isCheckedByDefault(): newvalues.contains(valueOfOption);
-		cad.append(this.getDisabled()).append(this.getClassId())
-				.append(isChecked ? RadioButton.CHECKED_ATTR_VALUE : PCMConstants.EMPTY_).
-		append(this.getOnClick()).append(this.getOnMouseOut()).append(this.getOnDbClick()).append(this.getOnMouseOver()).
-		append("/>").append("<label").append(this.getClassId()).append(" for=" + this.getId().replaceFirst(" id=", "") + "><span>").
-		append("&nbsp;").append(descrTraduced).append("&nbsp;").append("</span>").append("</label>");
-
+		input.append(this.getDisabled());
+		input.append(isChecked ? RadioButton.CHECKED_ATTR_VALUE : PCMConstants.EMPTY_);
+		input.append(this.getOnClick()).append(this.getOnMouseOut()).append(this.getOnDbClick()).append(this.getOnMouseOver()).append(">");
+		
+		//ends input
+		
+		cad.append(input);
+		cad.append("<span ").append(this.getClassId()).append("</span>").
+		
+		append("</label>");
+		
 		return cad.toString();
+		
+		
 	}
 
 	public boolean isCheckedByDefault() {
