5894c217ff6f4b70d76c6c7e9e8cd3ab15a82ec7
pdulce <pedro.dulce@babel.es>
2021-06-03 19:29:09 +0200

    ..

--- CDDGedeoner/src/test/java/org/gedeoner/selenium/MySeleniumTest.java
+++ CDDGedeoner/src/test/java/org/gedeoner/selenium/MySeleniumTest.java
@@ -7,8 +7,10 @@ import static org.openqa.selenium.support.ui.ExpectedConditions.presenceOfElemen
 
 import org.gedeoner.testcase.resource.MemoryData;
 import org.gedeoner.testcase.resource.WebdriverObject;
-import org.junit.Assert;
-import org.junit.jupiter.api.Test;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;	
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -18,11 +20,13 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 /***
  * Tests with SeleniumHQ WebDriver
  */
-public class GedeonesQuerySeleniumTest {
+public class MySeleniumTest {
 	
-	@Test
-	public void setup() {
-
+	@Test (groups = { "login", "query"})	
+	public void queryGedeones() {
+		
+		 System.out.println("testing queryGedeones WebDriver SELENIUM");
+		 
 		WebDriver driver = WebdriverObject.getWebDriverInstance();
 		MemoryData memoryData =  MemoryData.getUniqueInstance();
 		
@@ -78,6 +82,61 @@ public class GedeonesQuerySeleniumTest {
 			WebdriverObject.killDriverInstance();
 		}
 
+	}
+	
+	@Test (groups = { "login"})	
+	public void testLoginErrUser() {
+		makeAccessWithData("testLoginErrUser");
+	}
+	
+	@Test (groups = { "login"})	
+	public void testLoginErrPass() {
+		makeAccessWithData("testLoginErrPass");
+	}
+
+	
+	@Test (groups = { "login"})	
+	public void testLoginSucess() {
+		makeAccessWithData("testLoginSucess");
+	}
+
+	
+	
+	private void makeAccessWithData(String testMethod) {
+
+		WebDriver driver = WebdriverObject.getWebDriverInstance();
+		MemoryData memoryData =  MemoryData.getUniqueInstance();
+		
+		try {			
+			Map<String, String> datatest = memoryData.getDatosEscenarioTest(testMethod);
+			
+			driver.get(memoryData.getURL());
+			WebDriverWait waitForTree = new WebDriverWait(driver, Long.valueOf(10));
+			waitForTree.until(ExpectedConditions.visibilityOfElementLocated(By.id("entryForm.user")));
+			WebElement entryUserForm = waitForTree.until(presenceOfElementLocated(By.id("entryForm.user")));
+			WebElement entryPaswdForm = driver.findElement(By.name("entryForm.password"));
+
+			entryUserForm.sendKeys(datatest.get("entryForm.user"));
+			entryPaswdForm.sendKeys(datatest.get("entryForm.password"));
+
+			WebElement submitFormElement = driver.findElement(By.id(datatest.get(MemoryData.SUBMIT_ELEMENT)));
+			submitFormElement.click();
+
+			WebDriverWait waitForDivErrMsg = new WebDriverWait(driver, Long.valueOf(10));
+			String expression = datatest.get(MemoryData.ELEMENT_2_EVALUATE);
+			waitForDivErrMsg.until(ExpectedConditions
+					.visibilityOfElementLocated(expression.startsWith("/") ? By.xpath(expression) : By.id(expression)));
+			WebElement labelErr = waitForDivErrMsg.until(
+					presenceOfElementLocated(expression.startsWith("/") ? By.xpath(expression) : By.id(expression)));
+
+			Assert.assertTrue(labelErr.getText().contentEquals(datatest.get(MemoryData.VALUE_2_EVALUATE)));
+
+		} catch (Throwable exc) {
+			Assert.fail("Error in " + testMethod + ": " + exc.getMessage());
+
+		} finally {
+			WebdriverObject.killDriverInstance();
+		}
 	}
 
 }
--- CDDGedeoner/src/test/java/org/gedeoner/testcase/junit5/SampleJunitTest.java
+++ CDDGedeoner/src/test/java/org/gedeoner/testcase/junit5/SampleJunitTest.java
@@ -1,19 +1,33 @@
 package org.gedeoner.testcase.junit5;
 
-import org.junit.Assert;
-import org.junit.jupiter.api.Test;
+import org.testng.Assert;
+import org.testng.annotations.Test;	
 
-/***
- * Tests login feature for SeleniumHQ WebDriver
- */
-public class SampleJunitTest  {
+public class SampleJunitTest{
 	
-	@Test
-	public void setup() {		
+	 @Test(groups = { "functest", "checkintest" })
+
+	   public void testPrintMessage() {
+	      System.out.println("testing testsuite JUNIT");
+	      Assert.assertEquals("aaa", "aaa");
+	   }
+
+	
+	@Test (groups = { "functest"})	
+	public void sampleTest1() {		
 		try {			
 			Assert.assertTrue(Integer.parseInt("1") + Integer.parseInt("5") == 6);
 		} catch (Throwable exc) {
-			Assert.fail("Error in sample empty test :" + exc.getMessage());	
+			Assert.fail("Error in sample1 test :" + exc.getMessage());	
+		}
+	}
+	
+	@Test (groups = { "checkintest"})	
+	public void sampleTest2() {		
+		try {			
+			Assert.assertTrue(Integer.parseInt("5") + Integer.parseInt("15") == 20);
+		} catch (Throwable exc) {
+			Assert.fail("Error in sample2 test :" + exc.getMessage());	
 		}
 	}
 
