98a58fae09bf100f8796874e7785ccec7c933fdb
pdulce <pedro.dulce@babel.es>
2021-06-23 10:38:25 +0200

    .

--- CDD/src/main/java/org/cdd/service/dataccess/persistence/AnsiSQLAbstractDAOImpl.java
+++ CDD/src/main/java/org/cdd/service/dataccess/persistence/AnsiSQLAbstractDAOImpl.java
@@ -1479,8 +1479,7 @@ public abstract class AnsiSQLAbstractDAOImpl extends AbstractDAOImpl implements 
 						if (!fieldAbstract.isBlob()) {
 							if (fieldAbstract.isString()) {
 								if (fieldMaxlength >= MAX_FOR_CODES) {									
-									val_ = val_.replaceAll(PCMConstants.ASTERISC_SCAPED, "");
-									val_ = "%".concat(val_).concat("%");
+									val_ = val_.replaceAll(PCMConstants.ASTERISC_SCAPED, PCMConstants.PERCENTAGE_SCAPED);
 									val_ = getSpecialCharsConversion(val_);
 								}
 								pstmt_.setObject(contadorArgumentos + 1, val_);
