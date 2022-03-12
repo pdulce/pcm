8051f906336157f3bad84d7da2b5aa6ea10e4326
pedro.dulce <pedro.dulce@1SAG-J5688H2.babel-si.local>
2022-03-12 10:21:33 +0100

    .

--- CDDGedeoner/src/main/java/gedeoner/strategies/FiltrarPreSubdirecciones.java
+++ CDDGedeoner/src/main/java/gedeoner/strategies/FiltrarPreSubdirecciones.java
@@ -95,19 +95,7 @@ public class FiltrarPreSubdirecciones extends DefaultStrategyLogin {
 					IFieldValue fValuesServices = new FieldValue();
 					fValuesServices.setValues(colOfServicios);
 					newValuesFiltered.put(peticiones.searchField(ConstantesModelo.PETICIONES_10_SERVICIO).getName(), fValuesServices);
-					
-					Collection<String> colOfServiciosDest = new ArrayList<String>();
-					FieldViewSet serviciosDestCriteria = new FieldViewSet(servicios);
-					serviciosDestCriteria.setValue(ConstantesModelo.SERVICIO_3_UNIDAD_ORG, idorganismo);
-					List<FieldViewSet> listaServiciosDest = dataAccess.searchByCriteria(serviciosDestCriteria);
-					Iterator<FieldViewSet> iteServiciosDest = listaServiciosDest.iterator();
-					while (iteServiciosDest.hasNext()) {
-						FieldViewSet servic = iteServiciosDest.next();
-						colOfServiciosDest.add(String.valueOf((Long)servic.getValue(ConstantesModelo.SERVICIO_1_ID)));
-					}					
-					IFieldValue fValuesServicesDest = new FieldValue();
-					fValuesServicesDest.setValues(colOfServiciosDest);
-					newValuesFiltered.put(peticiones.searchField(ConstantesModelo.PETICIONES_12_SERVICIO_DESTINO).getName(), fValuesServicesDest);
+					newValuesFiltered.put(peticiones.searchField(ConstantesModelo.PETICIONES_12_SERVICIO_DESTINO).getName(), fValuesServices);
 														
 					Collection<String> colOfAplicativos = new ArrayList<String>();
 					FieldViewSet aplicativosCriteria = new FieldViewSet(aplicativos);
